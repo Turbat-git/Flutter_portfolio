3345b7ae194ab34d0e34ff5d9b4d81ff0f045711
TURBAT TURKHUU <TURKHT@tdm.local>
2026-02-27 11:26:40 +0800

    feat: add todo class and function for adding to todo

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,142 @@
 import 'package:flutter/material.dart';
+import './models/todo.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ToDoApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ToDoApp extends StatelessWidget {
+  const ToDoApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Todo App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TodoHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class TodoHomePage extends StatefulWidget {
+  const TodoHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<TodoHomePage> createState() => _TodoHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TodoHomePageState extends State<TodoHomePage> {
+  final List<Todo> todos = <Todo>[
+    Todo(
+      name: 'Get Food',
+      description:
+          'Stand in front of fridge for 10 minutes and decide I dont want anything in there',
+    ),
+    Todo(name: 'Solve World Hunger', description: 'Dont use fridge'),
+    Todo(
+      name: 'Catch the Fridge',
+      description: 'We are going to need a bigger boat',
+      complete: true,
+    ),
+  ];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final TextEditingController _controllerName = TextEditingController();
+  final TextEditingController _controllerDescription = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('My Fancy Todo App'),
+        backgroundColor: Theme.of(context).primaryColorLight,
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: ListView.builder(
+          itemCount: todos.length,
+          itemBuilder: (context, index) {
+            // This is what each todo will look like.
+            return Card(
+              color: todos[index].complete ? Colors.green : Colors.orange,
+              child: Padding(
+                padding: const EdgeInsets.all(5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(todos[index].name),
+                          Text(todos[index].description),
+                        ],
+                      ),
+                    ),
+                    Checkbox(
+                      value: todos[index].complete,
+                      onChanged: (value) {},
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: _openAddTodo,
+        child: Icon(Icons.add),
       ),
+    );
+  }
+
+  void _openAddTodo() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: EdgeInsets.fromLTRB(5, 9, 5, 0),
+                child: Text('Name'),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(5, 9, 5, 0),
+                child: TextFormField(controller: _controllerName),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(5, 9, 5, 0),
+                child: Text('Description'),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(5, 9, 5, 0),
+                child: TextFormField(controller: _controllerDescription),
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    todos.add(
+                      Todo(
+                        name: _controllerName.text,
+                        description: _controllerDescription.text,
+                      ),
+                    );
+                  });
+                  Navigator.pop(context);
+                  _controllerDescription.clear();
+                  _controllerName.clear();
+                },
+                child: const Text('Add'),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
---
+++ lib/models/todo.dart
@@ -0,0 +1,13 @@
+class Todo {
+  final String name;
+  final String description;
+  final bool complete;
+
+  Todo({required this.name, required this.description, this.complete = false});
+
+  @override
+  String toString() {
+    return '$name - ($description) [${complete ? 'Complete' : 'Not Complete'}]';
+  }
+}
+
